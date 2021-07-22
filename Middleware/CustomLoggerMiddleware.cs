6e75606520abf73c9d97a88dd6f305e1fecb71c5
Daniel Avila Avila <avila.daniel86@gmail.com>
2021-07-22 11:48:20 -0500

    Add project files.

---
+++ Middleware/CustomLoggerMiddleware.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public class CustomLoggerMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            Console.WriteLine("This is before request");
+            await next(context);
+            Console.WriteLine("This is after request");
+        }
+    }
+}
